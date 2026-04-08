86e524ad9f12f04c78be2f53e7b4c8a1f04287a4
Mr Deepak <dpkpspk@gmail.com>
2026-04-08 21:49:50 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://npcinje

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'npcinjection.usa72.wondercdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
